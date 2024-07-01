0f6df57d7644e537931e15fbd43d0ddac352345b
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-01 18:05:47 -0300

    Modelo para validar requisições

--- produtos/src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
+++ produtos/src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
@@ -1,6 +1,15 @@
 package br.com.api.produtos.modelo;
 
-//Classe será utilizada quando houver algum problema com requisições de cadastramento, seleções, alterações e excluções.
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+//Classe 'RespostaModelo' será utilizada quando houver algum problema com requisições de cadastramento, seleções, alterações e excluções.
+@Component
+@Getter
+@Setter
 public class RespostaModelo {
+
+    private String mensagem;
     
 }
