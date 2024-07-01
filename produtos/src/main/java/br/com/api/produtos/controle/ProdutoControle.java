74ce44b6d58eaaee97db0adcadb61b2a453df219
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-01 17:49:48 -0300

    Config. controle, responsavel pelas rotas da API

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,15 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //responsavel por criar rotas
 public class ProdutoControle {
+
+    @GetMapping("/") // rota de requisição, exibirá algum dado
+    public String rota (){
+        return "API de produtos Funcionando!";
+
+    }
     
 }
