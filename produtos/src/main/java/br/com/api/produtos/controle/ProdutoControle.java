71f0c420617878e1b654b7cc8139ed0d4fe346ac
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-03 00:37:29 -0300

    Implementando rota para remover produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -2,13 +2,16 @@ package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController //responsavel por criar rotas
@@ -16,6 +19,11 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
+
+    @GetMapping("/") // rota de requisição, exibirá algum dado
+    public String rota (){
+        return "API de produtos Funcionando!";
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {
@@ -32,9 +40,10 @@ public class ProdutoControle {
         return ps.cadastrarAlterar(pm, "cadastrar");
     }
 
-    @GetMapping("/") // rota de requisição, exibirá algum dado
-    public String rota (){
-        return "API de produtos Funcionando!";
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
     }
+
     
 }
