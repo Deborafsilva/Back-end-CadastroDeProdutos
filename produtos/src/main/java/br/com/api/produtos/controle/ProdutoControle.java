920bc88bdb753d9e71a35151fd87d12303f6f4c8
Deborafsilva <debora.fsilva@hotmail.com>
2024-07-02 22:41:28 -0300

    Rota para cadastrar produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
@@ -16,6 +19,11 @@ public class ProdutoControle {
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {
         return ps.listar();
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrar(pm);
     }
 
     @GetMapping("/") // rota de requisição, exibirá algum dado
